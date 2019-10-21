7d6fe6de2d0e1b8491e0c56017b8f8590b76b99a
[HarenKei] <seungmin1266@gmail.com>
2019-10-21 20:09:33 +0900

    블루툿스 제어 추가한 ino 파일

--- smartHome.ino
+++ smartHome.ino
@@ -1,7 +1,8 @@
-//대림대학교 컴퓨터 정보학부
+///대림대학교 컴퓨터 정보학부
 //Daelim University College dept.Computer Information
 
-
+#include <SoftwareSerial.h>
+#include <DHT11.h>
 #include <Wire.h>
 #include "DHT.h"
 #include <LiquidCrystal_I2C.h>  // Library for LCD
@@ -18,6 +19,7 @@ void bedTone();
 
 
 //기본적인 센서 핀
+
 int piezoPin = 8; // Piezo buzzer
 int irPin = A3;  // IR sensor
 int cdsPin = A2; //cds sensor
@@ -41,7 +43,7 @@ int B = 1976; // 시
 int notes[25] = { G, G, A, A, G, G, E, G, G, E, E, D, G, G, A, A, G, G, E, G, E, D, E, C };
 //학교종이 땡땡땡 멜로디
 int tempo = 200;
-
+boolean flag_a = 0;
 boolean flag = 1;
 boolean flag2 = 1;
 int numTones = 5;
@@ -50,13 +52,17 @@ int numTones = 5;
 
 // 가까울 시 울릴 경보의 멜로디 작성
 
+SoftwareSerial BTSerial(4,5);
+
 void setup()
 {
   // 시리얼 통신을 위해 통신속도(Baudrate)를 9600으로 설정
   Serial.begin(9600);
+  BTSerial.begin(9600);
   pinMode(piezoPin, OUTPUT); // 피에조 핀을 출력핀으로 설정
   dht.begin();
   TV.begin(); // I2C LCD의 기본 설정
+  
   TV.backlight();  // I2C LCD의 백라이트를 켜줌
   TV.setCursor(0, 0); // I2C LCD의 커서위치를 0, 0으로 설정(첫번째 줄, 첫번째 칸)
   //TV.print("Looking TV...."); // I2C LCD에 "Looking TV...." 메세지 출력
@@ -69,14 +75,18 @@ void setup()
 
 void loop()
 {
+
   digitalWrite(led_red, LOW);
   digitalWrite(led_green, LOW);
   digitalWrite(led_blue, LOW);
+ 
   
-  //TV.clear();
+  if(BTSerial.available()){
+    byte data = BTSerial.read();
+
   int cdsValue = analogRead(cdsPin);
-  Serial.print("조도 값 : "); //cds Value index
-  Serial.println(cdsValue);
+  Serial.print("조도 값 :");
+  Serial.println(cdsValue); //cds Value in
   int IRval = analogRead(irPin);
 
   float humidity = dht.readHumidity(); //humidity - 습도 측정 val
@@ -91,17 +101,75 @@ void loop()
   Serial.print("*C, ");
   Serial.print((int)humidity);
   Serial.println(" %");
+  Serial.print(IRval);
+    if((data == '1' && flag_a== 0)) {
+      flag2 = 1;
+      noTone(piezoPin);
+   
+     TV.backlight();
+      TV.print("hum : ");
+      TV.print(humidity);
+     TV.print(" %");
+      TV.setCursor(0,1);
+      TV.print("temp : ");
+      TV.print(temperature);
+      TV.print(" C");
+  
+      data=0;
+      flag_a = 1;
+      
+      
+    }
 
-  if(digitalRead(swsv) == LOW && flag2 == 1)
-  {
+
+    else if((data=='1' && flag_a ==1))
+    {
+      flag2 = 0;
+      TV.clear();
+      TV.noBacklight();
+      tone(piezoPin, 600, 1000);
+      delay(100);
+      flag_a=0;
+      data='0';
+      
+    }
+    
+
+  if (IRval < 100 && flag2 == 1) {          //TV가 켜져있고, 거리가 가깝다면 TV OFF
     flag2 = 0;
+    TV.clear();
+    TV.noBacklight();
+    tone(piezoPin, 600, 1000);
+    delay(100);
+  }
+
+  else if (IRval > 900 && flag2 == 0) {    //TV가 꺼져있고, 거리가 멀다면 TV ON
+    noTone(piezoPin);
+    flag2 = 1;
+    TV.backlight();
+    TV.print("hum : ");
+    TV.print(humidity);
+    TV.print(" %");
+    TV.setCursor(0,1);
+    TV.print("temp : ");
+    TV.print(temperature);
+    TV.print(" C");
+  }
+  
+
+  
+  //모터
+
+  if(digitalRead(swsv) == LOW && flag == 1)
+  {
+    flag = 0;
     servo.write(180);
     delay(1000);
   }
 
-  else if(digitalRead(swsv) == LOW && flag2 == 0)
+  else if(digitalRead(swsv) == LOW && flag == 0)
   {
-    flag2 = 1;
+    flag = 1;
     servo.write(0);
     delay(1000);
   }
@@ -121,27 +189,9 @@ void loop()
     }
  
 
-  if (IRval < 100 && flag == 1) {          //TV가 켜져있고, 거리가 가깝다면 TV OFF
-    flag = 0;
-    TV.clear();
-    TV.noBacklight();
-    tone(piezoPin, 600, 1000);
-    delay(100);
-  }
-
-  else if (IRval > 900 && flag == 0) {    //TV가 꺼져있고, 거리가 멀다면 TV ON
-    noTone(piezoPin);
-    flag = 1;
-    TV.backlight();
-    TV.print("hum : ");
-    TV.print(humidity);
-    TV.print(" %");
-    TV.setCursor(0,1);
-    TV.print("temp : ");
-    TV.print(temperature);
-    TV.print(" C");
   }
 }
+
 
 void bedTone() {
   for(int i = 0; i < 12; i++) {
